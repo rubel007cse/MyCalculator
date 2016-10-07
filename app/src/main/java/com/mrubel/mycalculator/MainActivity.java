76d8b43ead42edc28e94b42d96439b7c9b499bdd
mosharrofrubel <rubel007cse@gmail.com>
2016-10-08 01:34:25 +0600

    multiplicaiton working

--- app/src/main/java/com/mrubel/mycalculator/MainActivity.java
+++ app/src/main/java/com/mrubel/mycalculator/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView _output;
     Button n1,n2,n3,n4,n5,n6,n7,n8,n9,n0, _eq, _mi,_pl,_mu,_di,_cancel, _erase;
     ArrayList<String> my_list = new ArrayList<String>();
+    String showing_typed="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("1");
+                showing_typed = showing_typed+"1";
+                _output.setText(showing_typed);
 
             }
         });
@@ -57,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("2");
+                showing_typed = showing_typed+"2";
+                _output.setText(showing_typed);
             }
         });
 
@@ -64,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("3");
+                showing_typed = showing_typed+"3";
+                _output.setText(showing_typed);
             }
         });
 
@@ -71,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("4");
+                showing_typed = showing_typed+"4";
+                _output.setText(showing_typed);
             }
         });
 
@@ -78,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("5");
+                showing_typed = showing_typed+"5";
+                _output.setText(showing_typed);
             }
         });
 
@@ -85,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("6");
+                showing_typed = showing_typed+"6";
+                _output.setText(showing_typed);
             }
         });
 
@@ -92,6 +105,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("7");
+                showing_typed = showing_typed+"7";
+                _output.setText(showing_typed);
             }
         });
 
@@ -99,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("8");
+                showing_typed = showing_typed+"8";
+                _output.setText(showing_typed);
             }
         });
 
@@ -106,6 +123,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("9");
+                showing_typed = showing_typed+"9";
+                _output.setText(showing_typed);
 
             }
         });
@@ -114,6 +133,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("0");
+                showing_typed = showing_typed+"0";
+                _output.setText(showing_typed);
 
             }
         });
@@ -124,12 +145,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("+");
+                showing_typed = showing_typed+"+";
+                _output.setText(showing_typed);
             }
         });
         _mi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 my_list.add("-");
+                showing_typed = showing_typed+"-";
+                _output.setText(showing_typed);
             }
         });
 
@@ -137,6 +162,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("*");
+                showing_typed = showing_typed+"*";
+                _output.setText(showing_typed);
+
             }
         });
 
@@ -144,6 +172,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 my_list.add("/");
+                showing_typed = showing_typed+"/";
+                _output.setText(showing_typed);
+
             }
         });
 
@@ -152,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 my_list.clear();
                 _output.setText("");
+                showing_typed ="";
             }
         });
 
@@ -159,16 +191,32 @@ public class MainActivity extends AppCompatActivity {
         _eq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                // inserting my array into a string so that i can parse letter
                 String s = "";
                 if(!my_list.isEmpty()){
-                    for(int i=0; i < my_list.size(); i++){
-                        switch (my_list.get(i).charAt(i)){
-                            case '*' :
 
+                    for (int j =0; j < my_list.size(); j++){
+                        s = s+my_list.get(j);
+                    }
+                    Log.d("---------f",s);
+
+
+                    for(int i=0; i < my_list.size(); i++){
+                        switch (my_list.get(i).charAt(0)){
+                            case '*' :
+                                String p1[] = s.split("[*]");
+                                int q1 = Integer.parseInt(p1[0]);
+                                int q2 = Integer.parseInt(p1[1]);
+
+                                _output.setText((q1*q2)+"");
+                                showing_typed = "";
                                 break;
                         }
                     }
                 }
+                my_list.clear();
             }
         });
 
